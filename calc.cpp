078966f99cc37ec70590f7d6ab9fe973c924d8b8
Kevin Velasquez <kevinvelasquez@kevin.local>
2020-11-18 17:53:36 -0500

    modularizing code

--- calc.cpp
+++ calc.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <vector>
 #include <string>
 #include <sstream>
@@ -15,70 +16,84 @@ struct Calc {
 
 class CalcImpl : public Calc {
     public:
-        unordered_map<char, int> variables;
+        unordered_map<string, int> variables;
         
-        int evalExpr(const char *expr, int &result) {
+        int evalExpr(const char *expr, int *result) {
+            cout << result << endl;
             vector<string> tokens = tokenize(expr);
             switch(tokens.size()) {
                 case 1:
-                    if (tokens[0].empty()) {
-                        return 0;
-                    }
-                    char var = tokens[0].at(0);
-                    if ( (var >= 65 && var <= 90) || 
-                        (var >= 97 && var <= 122)) {
-                        if (variables.find(var) == variables.end()) {
-                            printf("This variable does not have an associated value\n");
-                        } else {
-                            return variables[var];
-                        }
-                    }
-                    char *endptr;
-                    int intVar = strtol(tokens[0], &endptr, 10);
-                    //Beginning pointer and endpointer will be the same if conversion failed
-                    if (endptr == tokens[0]) {
-                        return 0;
-                    } else {
-                        printf("%d\n", intVar);
-                    }
+                    evalOne(token[0], result);
                     break;
                 case 3:
-                    int second;
-                    if (tokens[1].size() > 1) {
-                        return 0;
-                    }
-                    second = sign_check(tokens[1].at(0));
-                    if (second == 0) {
-                        return 0;
-                    }
-                    int first;
-                    if (tokens[0].size() > 1) {
-                        char *endptr;
-                        first = strtol(tokens[0], &endptr, 10);
-                        if (endptr == tokens[0]) {
-                            return 0;
-                        }
-                    } else {
-                        char l = tokens[0].at(0);
-                        if (valid_letter(l)) {
-                            first = l;
-                        } else {
-                            return 0;
-                        }   
-                    }
-
-
-                    if (second == '/' && third == 0) {
-                        printf("Error\n");
-                        return 0;
-                    }
+                    evalThree(tokens, result);
                     break;
                 case 5:
                     break;
                 default:
                     return 0;
             }
+            return (result == NULL) ? 0 : 1;
         }
+
+        // Evaluate for one token
+        void evalOne(string token, int *result) {
+            // If string is alphabetical
+            if (is_alpha(token)) {
+                if (variables.find(token) == variables.end()) {
+                    cout << "This variable does not have an associated value" << endl;
+                } else {
+                    *result = variables[token];
+                }
+            }
+            // String has numbers in it
+            else {
+                char *endptr;
+                int num = strtol(tokens[0], &endptr, 10);
+                if (*endptr != '\0') {
+                    cout << "Error" << endl;
+                } else {
+                    *result = num;
+                }
+            }
+        }
+
+        // Evaluate for three tokens
+        void evalThree(vector<string> tokens, int *result) {
+            int ans = 0;
+            // If it is an assignment
+            if (tokens[1] == "=") {
+                // stuff
+            }
+            second = sign_check(tokens[1].at(0));
+            if (second == 0) {
+                return 0;
+            }
+            int first;
+            if (tokens[0].size() > 1) {
+                char *endptr;
+                first = strtol(tokens[0], &endptr, 10);
+                if (endptr == tokens[0]) {
+                    return 0;
+                }
+            } else {
+                char l = tokens[0].at(0);
+                if (valid_letter(l)) {
+                    first = l;
+                } else {
+                    return 0;
+                }   
+            }
+
+
+            if (second == '/' && third == 0) {
+                printf("Error\n");
+                return 0;
+            }
+        }
+
+
+
 
         // Splits a string into a vector of strings
         vector<string> tokenize(const string &expr) {
@@ -91,22 +106,22 @@ class CalcImpl : public Calc {
             return vec;
         }
 
-        bool valid_letter(string s) {
-            if ( (var >= 65 && var <= 90) || 
-                    (var >= 97 && var <= 122)) {
-                return true;
-            } else {
-                return false;
+        // Checks if string is alphabetical
+        bool is_alpha(string s) {
+            for (char const &c : s) {
+                if (!isalpha(c)) {
+                    return false;
+                }
             }
+            return true;
         }
 
-        int sign_check(char check) {
-            if (check == '=' || check == '+' ||
-                check == '-' || check == '/' ||
-                check == '*') {
+        // Checks if string is an operator, otherwise return "."
+        string is_op(string op) {
+            if (check == "+" || check == "-" || check == "/" || check == "*") {
                 return check;
-            } else {
-                return 0;
+            }
+            return ".";
         }
 }
 
@@ -121,5 +136,5 @@ extern "C" void calc_destroy(struct Calc *calc) {
 
 extern "C" int calc_eval(struct Calc *calc, const char *expr, int *result) {
     CalcImpl *obj = static_cast<CalcImpl *>(calc);
-    return obj->evalExpr(expr, *result);
+    return obj->evalExpr(expr, result);
 }
